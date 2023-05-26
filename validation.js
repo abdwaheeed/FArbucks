39273245d7c0d85eefa8d040d6db003ef886cf6b
abdul wahid <abdulwahid.aw211@gmail.com>
2023-05-26 12:30:28 +0700

    fix validation

--- validation.js
+++ validation.js
@@ -1,31 +1,31 @@
-const names = document.getElementById('name')
-const password = document.getElementById('password')
-const confirm = doucement.getElementById('confirm-password');
-const form = document.getElementById('form')
-const error = document.getElementById('error')
+const names = document.getElementById('name');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const form = document.getElementById('form');
+const phone = document.getElementById('phone')
+
+const phoneRegex = /^(08|\+62)[0-9]+$/
 
 form.addEventListener('submit', (e) => {
 
-    let messages = []
+  if (names.value === '' || names.value == null) {
+    alert('Name is required');
+  }
 
-    if(names.value === '' || names.value == null){
-        messages.push('Name is required')
-    }
+  if (password.value.length <= 5) {
+    alert('Password must be longer than 6 characters');
+  }
 
-    if(password.value.length <= 5){
-        messages.push('Password must be longer than 6 characters')
-    }
+  if (password.value.length >= 20) {
+    alert('Password must be less than 20 characters');
+  }
 
-    if(password.value.length >= 20){
-        messages.push('Password must be less than 20 characters')
-    }
+  if (confirmPassword.value !== password.value) {
+    alert('Passwords do not match');
+  }
 
-    if(confirm-password.value != password.value){
-        messages.push('Password doesnt match')
-    }
+  if(!phoneRegex.test(phone)){
+    alert('Phone number invalid')
+  }
 
-    if(messages.length > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-})
+});
